8912afce48aad05ee0af781428b399c420113dbb
Renan Souza <renansouza.sm@gmail.com>
2023-11-23 23:05:14 -0300

    Finalizada a parte da responsividade dos componentes e páginas

--- src/pages/Error/index.jsx
+++ src/pages/Error/index.jsx
@@ -1,9 +1,12 @@
 import "./style.css";
 
+import { Link } from "react-router-dom";
+
 export const Error = () => {
   return (
     <div className="errorContainer">
       <h1>404!</h1>
+      <p className="goBack">Voltar para <Link to={"/"}>Home</Link>?</p>
     </div>
   );
 };
