e1f9fac1ae78c2c127c92d1fd8a0bb001b065566
unknown <renansouza.sm@gmail.com>
2025-10-16 18:39:57 -0300

    build log error fix

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,22 +1,18 @@
 import { stripe } from "@/lib/stripe";
 import ProductDetail from "./_components/product-detail";
-import Stripe from "stripe";
 
-type ProductPageProps = {
-  params: {
-    id: string;
-  };
-  searchParams: { [key: string]: string | string[] | undefined };
-};
-
-export default async function ProductPage({ params }: ProductPageProps) {
-  const { id } = params;
+export default async function ProductPage({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
 
   const product = await stripe.products.retrieve(id, {
     expand: ["default_price"],
   });
 
-  const plainProduct = JSON.parse(JSON.stringify(product)) as Stripe.Product;
+  const plainProduct = JSON.parse(JSON.stringify(product));
 
   return <ProductDetail product={plainProduct} />;
 }
